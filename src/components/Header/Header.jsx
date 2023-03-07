bda77819a7668ee501f731fac5a073fe8d658c9e
AdiletAkmatov <adiletakmatovofficial@gmail.com>
2023-03-07 21:16:23 +0600

    fear: added header and icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AVATAR from '../../images/avatar.jpg'
 import LOGO from '../../images/logo.svg'
 import styles from '../../styles/Header.module.css'
 import { ROUTES } from '../../utils/routes'
+
 const Header = () => {
 	return (
 		<div className={styles.header}>
@@ -10,6 +12,50 @@ const Header = () => {
 				<Link to={ROUTES.HOME}>
 					<img src={LOGO} alt='logotype' />
 				</Link>
+			</div>
+
+			<div className={styles.info}>
+				<div className={styles.user}>
+					<div
+						className={styles.avatar}
+						style={{ backgroundImage: `url(${AVATAR})` }}
+					/>
+					<div className={styles.username}>Guest</div>
+				</div>
+
+				<form className={styles.form}>
+					<div className={styles.info}>
+						<svg className='icon'>
+							<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
+						</svg>
+					</div>
+
+					<div className={styles.input}>
+						<input
+							type='search'
+							name='search'
+							placeholder='Search for anyting...'
+							autoComplete='off'
+							onChange={() => {}}
+							value=''
+						/>
+					</div>
+					{false && <div className={styles.box}></div>}
+				</form>
+				<div className={styles.account}>
+					<Link to={ROUTES.HOME} className={styles.favourites}>
+						<svg className={styles['icon-fav']}>
+							<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
+						</svg>
+					</Link>
+
+					<Link to={ROUTES.CART} className={styles.cart}>
+						<svg className={styles['icon-cart']}>
+							<use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
+						</svg>
+						<span className={styles.count}>2</span>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
